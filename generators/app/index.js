0d38df2323ca13058fec160401a996d1a3e1d52f
Dejan Fajfar <dejan@fajfar.com>
2019-09-19 10:18:31 +0200

    Have to think

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,37 +1,40 @@
-'use strict';
+"use strict";
+
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    this.log(
-      yosay(`Welcome to the delightful ${chalk.red('ttouch template')} generator!`)
-    );
+	  this.log(
+			yosay(
+				`Welcome to the delightful ${chalk.red('ttouch template')} generator!`
+			)
+		);
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+		const prompts = [
+			{
+				type: 'confirm',
+				name: 'someAnswer',
+				message: 'Would you like to enable this option?',
+				default: true
+			}
+		];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+		return this.prompt(prompts).then(props => {
+			// To access props later use this.props.someAnswer;
+			this.props = props;
+		});
+	}
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+	writing() {
+		this.fs.copy(
+			this.templatePath("dummyfile.txt"),
+			this.destinationPath("dummyfile.txt")
+		);
+	}
 
-  install() {
-    this.installDependencies();
-  }
+	install() {
+		this.installDependencies();
+	}
 };
